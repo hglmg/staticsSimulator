bf0a24a231795b34e4503862b476956ab683d9f2
hglmg <01208991@pw.edu.pl>
2026-05-07 15:37:21 +0200

    konstruktory dla punktow/pretow

--- pret.cpp
+++ pret.cpp
@@ -1,6 +1,17 @@
 #include "pret.h"
+#include <cmath>
 
 Pret::Pret()
 {
+    pPocz = nullptr;
+    pKonc = nullptr;
+    dlugosc = 0;
+}
+
+Pret::Pret(Punkt* _pPocz, Punkt* _pKonc)
+{
+    pPocz = _pPocz;
+    pKonc = _pKonc;
+    dlugosc;
 
 }
